1c0ec27158d2229a014d204bc65d4869002ccc5b
Dimas Fathulyaqin <dimasfathulyaqin@gmail.com>
2026-06-09 11:36:27 +0700

    Update workflow and clean config

--- www/js/app.js
+++ www/js/app.js
@@ -19,7 +19,7 @@ if (isTauri) {
 } else {
     // Jika di browser, arahkan ke GitHub Releases
     document.getElementById('downloadAppBtn').addEventListener('click', () => {
-        window.open('https://github.com/username/postdim-repo/releases/latest', '_blank');
+        window.open('https://github.com/dimasfath1994/postdimtrial/releases/latest/download/app.exe', '_blank');
     });
 }
 
