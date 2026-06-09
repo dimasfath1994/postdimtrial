869cfc807fa5f4812d4ab337049e7331076f98d2
alihaja <75448679+dimasfath94@users.noreply.github.com>
2026-06-10 00:16:05 +0700

    feat: init request-engine

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ api tools development
 
 for tagging:
 
-git tag v0.4.9
-git push origin v0.4.9
+git tag v0.5.0
+git push origin v0.5.0
--- www/js/collab/services/request-dispatcher.js
+++ www/js/collab/services/request-dispatcher.js
@@ -3,7 +3,6 @@
  * Bertugas mengirim request ke server dan mengembalikan response mentah.
  */
 import { proxysendRequest } from "../../core/api/proxy-api.js";
-import { invoke } from "@tauri-apps/api/core";
 
 export class RequestDispatcher {
     
@@ -45,7 +44,7 @@ export class RequestDispatcher {
 
             // --- 3. EKSEKUSI ---
             if (window.__TAURI_INTERNALS__ !== undefined) {
-
+                const { invoke } = window.__TAURI_INTERNALS__?.core;
                 alert("MASUK TAURI");
                 // Konversi headers ke format array untuk Rust: [["Key", "Value"], ...]
                 const headerArray = Object.entries(config.headers);
