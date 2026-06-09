fd2453d07e373bf2ea5cb25c30f04dc72338eec4
alihaja <75448679+dimasfath94@users.noreply.github.com>
2026-06-09 17:09:19 +0700

    feat: init request-engine

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,12 +1,10 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// 1. Import module request_handler yang baru kita buat
 mod request_handler;
 
 fn main() {
     tauri::Builder::default()
-        // 2. Daftarkan command http_request agar bisa dipanggil dari JS
         .invoke_handler(tauri::generate_handler![
             request_handler::http_request
         ])
