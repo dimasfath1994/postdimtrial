b7d5c50daa0acb4f28554f7dd0b136e9337d6995
alihaja <75448679+dimasfath94@users.noreply.github.com>
2026-06-10 00:42:54 +0700

    feat: init request-engine

--- www/js/collab/services/request-dispatcher.js
+++ www/js/collab/services/request-dispatcher.js
@@ -44,7 +44,11 @@ export class RequestDispatcher {
 
             // --- 3. EKSEKUSI ---
             if (window.__TAURI_INTERNALS__ !== undefined) {
-                const { invoke } = window.__TAURI_INTERNALS__?.core;
+                // Coba deteksi berbagai variasi penempatan invoke
+                const invoke = window.__TAURI__?.invoke || 
+                window.__TAURI__?.core?.invoke || 
+                window.__TAURI_INTERNALS__?.invoke || 
+                window.__TAURI_INTERNALS__?.core?.invoke;
                 alert("MASUK TAURI");
                 // Konversi headers ke format array untuk Rust: [["Key", "Value"], ...]
                 const headerArray = Object.entries(config.headers);
