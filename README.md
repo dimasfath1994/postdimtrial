c03cb1c1addc3cd22d94c31c5b0013def3951d3e
alihaja <75448679+dimasfath94@users.noreply.github.com>
2026-06-21 23:46:23 +0700

    feat: change endpoint

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ api tools development
 
 for tagging:
 
-git tag v0.8.4
-git push origin v0.8.4
+git tag v0.8.6
+git push origin v0.8.6
--- www/js/core/api/api-config.js
+++ www/js/core/api/api-config.js
@@ -1,8 +1,8 @@
 export const API_BASE_URL =
-  "https://meanwhile-bus-aging-retired.trycloudflare.com/api";
+  "https://henderson-paragraph-americans-wagon.trycloudflare.com/api";
   //"http://192.168.0.104:3000/api";
 
 
 export const WS_BASE_URL = 
-"wss://meanwhile-bus-aging-retired.trycloudflare.com/api";
+"wss://henderson-paragraph-americans-wagon.trycloudflare.com/api";
 //"ws://192.168.0.104:3000/api";
